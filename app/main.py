bc42a16046e83d626d59a52b36c9ddeb1881b765
Sai-Santhan <dsaisanthan@gmail.com>
2022-09-07 10:08:38 +0530

    Sync Cassandra Tables via FastAPI on Startup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
+from app.core import (
+    db
+    # config
+)
+from cassandra.cqlengine.management import sync_table
+from app.users.models import User
+
+DB_SESSION = None
 
 app = FastAPI()
+# settings = config.get_settings()
+
+
+@app.on_event("startup")
+def on_startup():
+    global DB_SESSION
+    DB_SESSION = db.get_session()
+    sync_table(User)
 
 
 def start():
