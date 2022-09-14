030763ae46a82d16e09094883ee28d236e57f033
Sai-Santhan <dsaisanthan@gmail.com>
2022-09-14 20:23:00 +0530

    Added router for video sub module

---
+++ app/videos/routers.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from app.core.shortcuts import render
+from app.videos.models import Video
+
+router = APIRouter(
+    prefix="/videos"
+)
+
+
+@router.get("/", response_class=HTMLResponse)
+def video_list_view(request: Request):
+    q = Video.objects.all().limit(100)
+    context = {
+        "object_list": q,
+    }
+    return render(request, "videos/list.html", context)
