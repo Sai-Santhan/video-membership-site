ad3f4c7772409fae606bc3335ee2917e4d3972b1
Sai-Santhan <dsaisanthan@gmail.com>
2022-09-15 10:41:26 +0530

    Added video create and detail views with login_required decorator and respective templates.

--- app/videos/routers.py
+++ app/videos/routers.py
@@ -2,11 +2,18 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 
 from app.core.shortcuts import render
+from app.users.decorators import login_required
 from app.videos.models import Video
 
 router = APIRouter(
     prefix="/videos"
 )
+
+
+@router.get("/create", response_class=HTMLResponse)
+@login_required
+def video_create_view(request: Request):
+    return render(request, "videos/create.html")
 
 
 @router.get("/", response_class=HTMLResponse)
@@ -16,3 +23,8 @@ def video_list_view(request: Request):
         "object_list": q,
     }
     return render(request, "videos/list.html", context)
+
+
+@router.get("/detail", response_class=HTMLResponse)
+def video_detail_view(request: Request):
+    return render(request, "video/detail.html")
