acb6d1006cdbd11d392b3f0f383e3727425d1d9f
Sai-Santhan <dsaisanthan@gmail.com>
2022-09-16 23:23:26 +0530

    Added render method for generating embed iframe to render.

--- app/videos/models.py
+++ app/videos/models.py
@@ -2,7 +2,7 @@ import uuid
 
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
-
+from app.core.shortcuts import templates
 from app.core.config import get_settings
 from app.users.exceptions import InvalidUserIDException
 from app.users.models import User
@@ -26,6 +26,13 @@ class Video(Model):
 
     def __repr__(self):
         return f"Video(title={self.title}, host_id={self.host_id}, host_service={self.host_service})"
+
+    def render(self):
+        basename = self.host_service
+        template_name = f"/videos/renderers/{basename}.html"
+        context = {"host_id": self.host_id}
+        t = templates.get_template(template_name)
+        return t.render(context)
 
     def as_data(self):
         return {
