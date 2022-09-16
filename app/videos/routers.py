25e769c9b5fe429cc4c740edc8bf0732ee05387d
Sai-Santhan <dsaisanthan@gmail.com>
2022-09-16 12:33:25 +0530

    Video create post view with Form

--- app/videos/routers.py
+++ app/videos/routers.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse
 
-from app.core.shortcuts import render
+from app.core import utils
+from app.core.shortcuts import render, redirect
 from app.users.decorators import login_required
 from app.videos.models import Video
+from app.videos.schemas import VideoCreateSchema
 
 router = APIRouter(
     prefix="/videos"
@@ -14,6 +16,33 @@ router = APIRouter(
 @login_required
 def video_create_view(request: Request):
     return render(request, "videos/create.html")
+
+
+@router.post("/create", response_class=HTMLResponse)
+@login_required
+def video_create_post_view(request: Request,
+                           title: str = Form(),
+                           url: str = Form()):
+    raw_data = {
+        "title": title,
+        "url": url,
+        "user_id": request.user.username
+    }
+    data, errors = utils.valid_schema_data_or_error(raw_data, VideoCreateSchema)
+
+    redirect_path = data.get("path") or "/videos/create"
+
+    context = {
+        "data": data,
+        "errors": errors,
+        "title": title,
+        "url": url,
+    }
+
+    if len(errors) > 0:
+        return render(request, "videos/create.html", context, status_code=400)
+
+    return redirect(redirect_path)
 
 
 @router.get("/", response_class=HTMLResponse)
