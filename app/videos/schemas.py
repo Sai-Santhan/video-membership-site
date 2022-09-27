0e7b7448f6112b4da53c69cc5198ee719cef444d
Sai-Santhan <dsaisanthan@gmail.com>
2022-09-27 14:14:59 +0530

    Video Edit Schema to validate url.

--- app/videos/schemas.py
+++ app/videos/schemas.py
@@ -49,3 +49,15 @@ class VideoCreateSchema(BaseModel):
         #     video_obj.title = title
         #     video_obj.save()
         return video_obj.as_data()
+
+
+class VideoEditSchema(BaseModel):
+    url: str
+    title: str
+
+    @validator("url")
+    def validate_youtube_url(cls, v, values, **kwargs):
+        video_id = extract_video_id(v)
+        if video_id is None:
+            raise ValueError(f"{v} is not a valid YouTube URL")
+        return v
